87e635f1bba53042855e841cd0fbd9bb230ebe08
Nwaeze Joshua <nwaezejoshua@gmail.com>
2022-08-14 14:45:59 +0100

    Update index.js

--- Encryption Program/index.js
+++ Encryption Program/index.js
@@ -1,5 +1,7 @@
+
+
 const NodeRSA = require('node-rsa');
-const key = new NodeRSA({b: 1024});
+const key = new NodeRSA({ b: 1024 });
  
 //create a secret text that will be encrypted
 let secret = "abcdefghijklmnopqrstuvwxyz"
@@ -13,10 +15,11 @@ console.log(encryptedString);
 //let's decrypt the alphabets with the given decryption keys 
 // £*%&><!)"(@abcdefghijklmno
 
-private_key = '£*%-&><!)"(-@-abcd-efgh-ijkl-mno';
+// private_key = '£*%-&><!)"(-@-abcd-efgh-ijkl-mno';
 
 
-let key_private = new NodeRSA(private_key);
+// let key_private = new NodeRSA(private_key);
 
-const decryptedString = key_private.decrypt(encryptedString, 'utf8');
+//let's decrypt the alphabets with the given decryption key 
+const decryptedString = key.decrypt(encryptedString, 'utf8');
  console.log(decryptedString);
